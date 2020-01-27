411a859cb4b4355abd32b2ce92899a9c79ffa03c
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-01-27 09:25:49 -0500

    updated electric bill with conditions

--- ElectricBillNew.cs
+++ ElectricBillNew.cs
@@ -6,11 +6,14 @@ namespace _2020._01._27
     {
         static void Main(string[] args)
         {
-            const int last_meter = 39803;
+             const int last_meter = 39803;
             const decimal monthly_fee = 10.00M;
+            const decimal discount = 0.10M;
             int total_used;
             decimal charge_per_unit = 0.095M;
-            decimal total_bill, total_energy_charge;
+            decimal total_bill, total_energy_charge, amount_after_discount, total_bill_after_discount;
+            
+
 
             Console.Write("Enter your current meter reading: ");
             var current_meter = Convert.ToInt32(Console.ReadLine());
@@ -20,6 +23,7 @@ namespace _2020._01._27
                 Console.WriteLine("You have to pay $100 more as extra charges.");
                 var pay_more = 100;
                 total_used = current_meter - last_meter;
+                Console.WriteLine("You'll get 10 percent discount");
                 total_energy_charge = total_used * charge_per_unit;
                 total_bill = total_energy_charge + monthly_fee + pay_more;
                 Console.WriteLine("Your total bill is {0:c} including your extra charges that is {1:c} .", total_bill, pay_more);
@@ -27,14 +31,26 @@ namespace _2020._01._27
             }
             else
             {
+
                 Console.WriteLine("You're good. ");
                 total_used = current_meter - last_meter;
-                total_energy_charge = total_used * charge_per_unit;
-                total_bill = total_energy_charge + monthly_fee;
-                Console.WriteLine("Your total bill is {0:c} .", total_bill);
-            }
+                if (total_used == 1000)
+                {
 
-            
+                    total_energy_charge = total_used * charge_per_unit;
+                    total_bill = total_energy_charge + monthly_fee;
+                    amount_after_discount = total_bill * discount;
+                    total_bill_after_discount = total_bill - amount_after_discount;
+                    Console.WriteLine("Your total bill is {0:c} .", total_bill_after_discount);
+                }
+                else
+                {
+                    total_energy_charge = total_used * charge_per_unit;
+                    total_bill = total_energy_charge + monthly_fee;
+                    Console.WriteLine("Your total bill is {0:c} .", total_bill);
+                }
+
+            }
         }
     }
 }
