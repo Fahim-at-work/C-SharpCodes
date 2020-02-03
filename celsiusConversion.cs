1895b56216ca1a45f8bdca1b723804a427cd2902
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-02-02 22:57:40 -0500

    added Celsius to kelvin and Fahrenheit

---
+++ celsiusConversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter degree celsius to calculate kelvin and fahrenheit: ");
+            var celsius = Convert.ToDecimal(Console.ReadLine());
+
+            decimal kelvin, fahrenheit;
+
+            kelvin = celsius + 273.15M;
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("Kelvin: {0}", kelvin);
+            Console.WriteLine("Fahrenheit: {0}", fahrenheit);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
