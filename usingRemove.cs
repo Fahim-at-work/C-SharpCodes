e3bd545260c82f27258693a8725304b523147b96
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-02-04 14:22:04 -0500

    use of remove method on string

---
+++ usingRemove.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(removingChar("FahimShariff", 1));
+            Console.WriteLine(removingChar("FahimShariff", 4));
+            Console.WriteLine(removingChar("FahimShariff", 6));
+
+        }
+
+        public static string removingChar(string s, int i)
+        {
+            return s.Remove(i, 3);
+        }
+    }
+}
