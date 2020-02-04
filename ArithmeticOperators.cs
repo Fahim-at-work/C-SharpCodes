3dfd14c8281b7f28d61cb74d04195206fc97ba25
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-02-04 13:54:47 -0500

    Arithmetic Operation using switch case

---
+++ ArithmeticOperators.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //taking input from the user
+            Console.Write("Enter first number: ");
+            var num_one = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Select an operator +, -, *, /, % : ");
+            var operator_entered = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            var num_two = Convert.ToDecimal(Console.ReadLine());
+
+            //Checking the cases and printing the output
+            
+            switch (operator_entered)
+            {
+                case "+":
+                    Console.WriteLine(num_one + num_two);
+                    break;
+
+                case "-":
+                    Console.WriteLine(num_one - num_two);
+                    break;
+
+                case "*":
+                    Console.WriteLine(num_one * num_two);
+                    break;
+
+                case "/":
+                    Console.WriteLine(num_one / num_two);
+                    break;
+
+                case "%":
+                    Console.WriteLine(num_one % num_two);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
