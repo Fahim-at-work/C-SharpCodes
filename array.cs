514020aa28047f14923a2e8b4316787428211843
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-02-10 10:01:45 -0500

    reverse of numbers in array in class activity

---
+++ array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2020._02._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+		   
+		    int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Original: ");
+            foreach (int i in num)
+            {
+                Console.Write(i + " ");
+            }
+
+            Array.Reverse(num);
+            Console.WriteLine("\n\nReversed: ");
+            foreach (int j in num)
+            {
+                Console.Write(j + " ");
+            }
+
+            
+        }
+    }
+}
