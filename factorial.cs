03ab2881ced6ecd2102309c26e5fc93f229f466c
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-02-10 09:59:46 -0500

    factorial of a number in class activity

---
+++ factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2020._02._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+		   
+		    Console.Write("Enter a number you want to calculate the factorial of: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine("The factorial of {0} is {1}", num, fact);
+
+
+            
+        }
+    }
+}
