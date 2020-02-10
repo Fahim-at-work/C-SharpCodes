912877267ee7b75e3938dc5cb95379c2941cb1b8
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-02-09 21:56:39 -0500

    Add files via upload

---
+++ student-marks-array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students are there in the class? ");
+            var number_of_students = Convert.ToInt32(Console.ReadLine());
+
+
+            var student_name = new string[number_of_students];
+            var student_marks = new int[number_of_students];
+
+            for (int i = 0; i < number_of_students; i++)
+            {
+                Console.Write("Enter the student's name: ");
+                student_name[i] = Console.ReadLine();
+
+                Console.Write("Enter the student's grade: ");
+                student_marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < number_of_students; i++)
+            {
+                Console.WriteLine("Student Name: {0}", student_name[i]);
+                Console.WriteLine("Student Marks: {0}", student_marks[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
