d09da65ad84538a3167c55e5405e80ed51165a29
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-03-23 20:51:19 -0400

    Add files via upload

---
+++ reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+
+
+            String outputString = "";
+            Console.WriteLine("Enter a string to find the reverse");
+            var inputString = Console.ReadLine();
+
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                outputString += inputString[i];
+            }
+
+            Console.WriteLine($"The reverse of the string is {outputString}");
+            Console.ReadLine();
+        }
+    }
+}
+
+
