863cd7f9b3ec87a049398e804f027df7b6ccb095
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-03-23 20:48:53 -0400

    Add files via upload

---
+++ wordCount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {
+
+        static void wordsCount(String x)
+        {
+            
+            int i, wordCount, letterCount;
+            letterCount = 0;
+            wordCount = 1;
+            while (letterCount <= x.Length - 1)
+            {
+                if (x[letterCount] == ' ' || x[letterCount] == '\n' || x[letterCount] == '\t')
+                {
+                    wordCount++;
+                }
+
+                letterCount++;
+            }
+
+            Console.WriteLine("The number of words are {0}", wordCount);
+            Console.WriteLine("The number of letters are {0}", letterCount);
+        }
+
+        
+        static void Main(string[] args)
+        {
+
+
+            string inputString;
+            Console.Write("Input the string : ");
+            inputString = Console.ReadLine();
+
+            wordsCount(inputString);
+
+            Console.ReadLine();
+        }
+    }
+}
