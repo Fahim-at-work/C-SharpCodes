0f5c9e9c7f9335c311f1d6c181fafd91dfd2ba9c
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-03-23 21:13:43 -0400

    Add files via upload

---
+++ allCount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+
+            int alphabetCount = 0, digitCount = 0, specialCount = 0;
+            String inputString;
+
+            Console.WriteLine("Enter a string with special characters");
+            inputString = Console.ReadLine();
+
+            for(int i = 0; i < inputString.Length; i++)
+            {
+                if ((inputString[i] >= 'a' && inputString[i] <= 'z') || (inputString[i] >= 'A' && inputString[i] <= 'Z'))
+                {
+                    alphabetCount++;
+                }
+                else if(inputString[i] >= '0' && inputString[i] <= '9')
+                {
+                    digitCount++;
+                }
+                else
+                {
+                    specialCount++;
+                }
+            }
+
+            Console.WriteLine("The number of alphabets are {0}", alphabetCount);
+            Console.WriteLine("The number of digits are {0}", digitCount);
+            Console.WriteLine("The number of special characters are {0}", specialCount);
+
+        }
+    }
+}
+
+
